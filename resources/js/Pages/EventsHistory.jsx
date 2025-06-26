24718233762e3e9dcee75db094db80f8c2c0c874
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2025-06-26 11:32:33 +0200

    events history sections updated to active, future and ended

--- resources/js/Pages/EventsHistory.jsx
+++ resources/js/Pages/EventsHistory.jsx
@@ -16,7 +16,22 @@ export default function EventsHistory({ auth, events }) {
             <div className="container mt-4">
                 <div className="card">
                     <div className="card-body">
-                        <h4 className="card-title">Subscribed</h4>
+                        <h4 className="card-title">Active events</h4>
+                        <ul className="list-group list-group-flush">
+                            {
+                                events
+                                    .filter(event => new Date(event.start_date) <= new Date() && new Date(event.end_date) > new Date())
+                                    .map(event => (
+                                        <ParticipantEventRow
+                                            key={event.id}
+                                            event={event}
+                                        />
+                                    ))
+                            }
+                        </ul>
+                    </div>
+                    <div className="card-body">
+                        <h4 className="card-title">Future events</h4>
                         <ul className="list-group list-group-flush">
                             {
                                 events
@@ -31,11 +46,11 @@ export default function EventsHistory({ auth, events }) {
                         </ul>
                     </div>
                     <div className="card-body">
-                        <h4 className="card-title">History</h4>
+                        <h4 className="card-title">Finished events</h4>
                         <ul className="list-group list-group-flush">
                             {
                                 events
-                                    .filter(event => new Date(event.start_date) <= new Date())
+                                    .filter(event => new Date(event.end_date) <= new Date())
                                     .map(event => (
                                         <ParticipantEventRow
                                             key={event.id}
