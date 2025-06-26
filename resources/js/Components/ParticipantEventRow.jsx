836850b9ee91c8ad0a79e61d4f3131a0df6ad2bc
Daniel Hoyos Celdrán <danhoycel@gmail.com>
2025-06-26 11:57:19 +0200

    participant event row style updated

--- resources/js/Components/ParticipantEventRow.jsx
+++ resources/js/Components/ParticipantEventRow.jsx
@@ -1,15 +1,25 @@
 import { Link } from "@inertiajs/react";
+import IconText from "./iconText";
+
+import clock from "../../assets/icons/clock.svg";
+import location from "../../assets/icons/map-pin.svg";
 
 export default function ParticipantEventRow({ event }) {
+    const dates = new Date(event.start_date).toLocaleDateString() + "-" + new Date(event.end_date).toLocaleDateString()
     return (
         <div className="card mx-3 my-1">
             <Link href={route("event.participant.show", { event_id: event.id })} className="text-decoration-none text-dark">
                 <div className="card-body p-2 px-3">
-                    <div className="row d-flex align-items-center">
+                    <div className="container d-flex align-items-center justify-content-between">
                         <div className="col">
                             <h5>{event.name}</h5>
                         </div>
-                        <div className="col text-end">{new Date(event.start_date).toLocaleDateString()} - {new Date(event.end_date).toLocaleDateString()}</div>
+                        <div className="col d-flex align-items-center justify-content-end">
+                            <div className="container d-flex align-items-center justify-content-end gap-4">
+                                <div className=""><IconText icon={clock} text={dates} /></div>
+                                <div className=""><IconText icon={location} text={event.location} /></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Link>
--- resources/js/Components/iconText.jsx
+++ resources/js/Components/iconText.jsx
@@ -1,7 +1,7 @@
 export default function IconText({ icon, text, className = "", textClassName = "" }) {
     return (
         <div className={`d-flex align-items-center ${className} p-0 m-0`}>
-            <img src={icon} alt={text} className="me-2" style={{ width: "16px", height: "16px" }} />
+            <img src={icon} alt={text} className="me-1" style={{ width: "16px", height: "16px" }} />
             <span className={textClassName} >{text}</span>
         </div>
     );
